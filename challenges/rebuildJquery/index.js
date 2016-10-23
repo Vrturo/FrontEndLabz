4a74b8c40b2df871c8fe711e9f1db5df2d161f7f
Arturo Perez <VrturoPerez@gmail.com>
2016-10-23 02:40:33 -0700

    add dom module

--- challenges/rebuildJquery/index.js
+++ challenges/rebuildJquery/index.js
@@ -1,13 +1,35 @@
 class SweetSelector {
 
-  select(str){
-    if( str[0] === '#' ) return document.getElementById( str.substring(1, str.length) );
-    if( str[0] === '.' ) return document.getElementsByClassName( str.substring(1, str.length) );
-    return document.getElementsByTagName( str );
-  }
+    select(str) {
+        if( str[0] === '#' ) return document.getElementById( str.substring(1, str.length) );
+        if( str[0] === '.' ) return document.getElementsByClassName( str.substring(1, str.length) );
+        return document.getElementsByTagName( str );
+    }
 
 }
 var ss = new SweetSelector();
-console.log(ss.select('.container'))
-console.log(ss.select('#fade1'))
-console.log(ss.select('div'))
+// console.log(ss.select('.container'))
+// console.log(ss.select('#fade1'))
+// console.log(ss.select('div'))
+
+class DOM{
+
+    hide( element ) {
+        element = element.length <= 1 ? [element] : element;
+        for( var i = 0; i < element.length; i++ ){
+            element[i].style.display = 'none';
+        }
+    }
+
+    show( element ) {
+        element = element.length <= 1 ? [element] : element;
+        for( var i = 0; i < element.length; i++ ){
+            element[i].style.display = 'block';
+        }
+    }
+}
+
+var d = new DOM();
+var div = ss.select('div')
+console.log(d.hide(div))
+console.log(d.show(div))
