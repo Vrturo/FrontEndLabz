2f093d4265ff2967cf003ebeec4c4a8c2d8d36f2
Arturo Perez <VrturoPerez@gmail.com>
2016-10-22 23:52:12 -0700

    create a css library

--- challenges/rebuildJquery/index.js
+++ challenges/rebuildJquery/index.js
@@ -1,1 +1,13 @@
-console.log('hey')
+class SweetSelector {
+
+  select(str){
+    if( str[0] === '#' ) return document.getElementById( str.substring(1, str.length) );
+    if( str[0] === '.' ) return document.getElementsByClassName( str.substring(1, str.length) );
+    return document.getElementsByTagName( str );
+  }
+
+}
+var ss = new SweetSelector();
+console.log(ss.select('.container'))
+console.log(ss.select('#fade1'))
+console.log(ss.select('div'))
