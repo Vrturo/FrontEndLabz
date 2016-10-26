50cc77d4c602237aacc805464dbf60bfc9413beb
Arturo Perez <VrturoPerez@gmail.com>
2016-10-26 11:55:59 -0700

    add concepts directory and ajax file

--- challenges/rebuildJquery/index.js
+++ challenges/rebuildJquery/index.js
@@ -1,11 +1,9 @@
 class SweetSelector {
-
     select(str) {
         if( str[0] === '#' ) return document.getElementById( str.substring(1, str.length) );
         if( str[0] === '.' ) return document.getElementsByClassName( str.substring(1, str.length) );
         return document.getElementsByTagName( str );
     }
-
 }
 var ss = new SweetSelector();
 // console.log(ss.select('.container'))
