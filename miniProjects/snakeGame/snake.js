974e3a0c3797891d689e52dbc1905bea69b001a7
Arturo Perez <VrturoPerez@gmail.com>
2016-11-08 18:20:41 -0800

    attempt to move snake

--- miniProjects/snakeGame/snake.js
+++ miniProjects/snakeGame/snake.js
@@ -24,32 +24,68 @@ class Game {
     this.snake = new Snake
   }
 
+  generatefood(){
+      var x = Math.floor(Math.random() * 19);
+      var y = Math.floor(Math.random() * 19);
+      $('#c_' + x + '_' + y).addClass('food');
+      this.food = x + '_' + y;
+  }
+
 }
 
 class Snake {
   constructor(){
-    this.direction = 'r';
+    this.direction = 4;
     this.position = ["20_20", "20_19", "20_18"]
     this.tail = this.position.pop();
     this.head = this.position[0].split('_');
     var row = parseInt(this.head[0]);
     var col = parseInt(this.head[1]);
     $('#c_' + row + '_' + col).addClass('snake');
+    this.location = row + "_" + col;
+    switch(this.direction){
+      case 1: row += 1; break;
+      case 2: col -= 1; break;
+      case 3: row -= 1; break;
+      case 4: col += 1; break;
+    }
+  }
+
+  go(){
+    if (this.location === super.food) {
+      this.position.push(tail);
+      var points = super.score += 1;
+
+      if (points % 100 == 0) {
+        $('#level').text(super.level += 1);
+        if (super.level == 5) {
+          $('.cells').css('outline','none')
+        }
+        if (super.speed > 0) { super.speed -= 20; }
+      }
+      $('#score').text(super.points);
+      $('#c_'+tail).addClass('snake');
+      $('#c_'+super.food).removeClass('food');
+      generatefood();
+    }
+    this.position.unshift(location);
   }
 
 }
 
 
-function generatefood(){
-    var x = Math.floor(Math.random() * 19);
-    var y = Math.floor(Math.random() * 19);
-    $('#c_' + x + '_' + y).addClass('food');
-}
 
-function startGame(){
-  var newGame = new Game();
-  newGame.renderGrid();
-  newGame.renderSnake()
-}
+var newGame = new Game;
+newGame.renderGrid();
+newGame.renderSnake();
+newGame.snake.go()
 
-startGame();
+$(document).keypress(function(e){
+    e.preventDefault();
+    switch(e.key){
+      case  "ArrowDown": dir = 1; break;
+      case  "ArrowLeft": dir = 2; break;
+      case    "ArrowUp": dir = 3; break;
+      case "ArrowRight": dir = 4; break;
+    }
+});
