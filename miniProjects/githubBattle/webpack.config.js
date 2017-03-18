0ebb529419a72d5552afd98fd19a676e32534653
Arturo Perez <VrturoPerez@gmail.com>
2017-03-17 20:18:14 -0700

    add babel

--- miniProjects/githubBattle/webpack.config.js
+++ miniProjects/githubBattle/webpack.config.js
@@ -1,3 +1,9 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+	template: __dirname + '/app/index.html',
+	filename: 'index.html',
+	inject: 'body'
+})
 module.exports = {
 	entry: [
 		'./app/index.js'
@@ -10,5 +16,6 @@ module.exports = {
 		loaders: [
 			{test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'}
 		]
-	}
+	},
+	plugins: [HtmlWebpackPlugin]
 }
