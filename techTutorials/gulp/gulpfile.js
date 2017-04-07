2aee0d60d3d1dd6545908aaf33143cb1b4a3e39c
Arturo Perez <VrturoPerez@gmail.com>
2017-04-07 11:49:17 -0700

    create app skeleton

--- techTutorials/gulp/gulpfile.js
+++ techTutorials/gulp/gulpfile.js
@@ -1,6 +1,14 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
 gulp.task('hello', function() {
   // Stuff here
   console.log('Hello Art');
+});
+
+
+gulp.task('sass', function(){
+  return gulp.src('source-files')
+    .pipe(sass()) // Using gulp-sass
+    .pipe(gulp.dest('destination'))
 });
