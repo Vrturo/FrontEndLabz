c30754a1ea4f9f0c3f32e18852baea4db13bdff9
Arturo Perez <VrturoPerez@gmail.com>
2017-04-07 12:27:57 -0700

    create a server and have it listen to changes dynamically for sass files

--- techTutorials/gulp/gulpfile.js
+++ techTutorials/gulp/gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
 gulp.task('hello', function() {
   // Stuff here
@@ -7,7 +8,25 @@ gulp.task('hello', function() {
 });
 
 gulp.task('sass', function() {
-  return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
-    .pipe(sass())
-    .pipe(gulp.dest('app/css'))
+	return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
+		.pipe(sass())
+		.pipe(gulp.dest('app/css'))
+		.pipe(browserSync.reload({
+      stream: true
+    }))
+})
+
+gulp.task('watch', ['browserSync', 'sass'], function(){
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload); 
+  // Other watchers
+})
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
 })
